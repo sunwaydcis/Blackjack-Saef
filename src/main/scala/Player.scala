6438f1b4bb93cc36693cbc0b307aca82f08d5719
niceguysaef <138495045+niceguysaef@users.noreply.github.com>
2024-08-17 03:29:09 +0800

    Update Player.scala
    added the 'hit' and 'stand' feature for the player allowing them to choose what they want to do during their turn

--- src/main/scala/Player.scala
+++ src/main/scala/Player.scala
@@ -1,3 +1,5 @@
+import scala.io.StdIn.readLine
+
 abstract class Player(val name: String) {
   val hand: Hand = new Hand()
 
@@ -6,6 +8,29 @@ abstract class Player(val name: String) {
 
 class HumanPlayer(name: String) extends Player(name) {
   override def playTurn(deck: Deck): Unit = {
+    var continue = true
+
+    while (continue && hand.value < 21) {
+      println(s"Your current hand: ${hand} (Value: ${hand.value})")
+      println("Do you want to 'hit' or 'stand'?")
+
+      readLine().toLowerCase match {
+        case "hit" =>
+          hand.addCard(deck.dealCard().get)
+          println(s"You drew a card: ${hand.getCards.head}")
+          if (hand.value > 21) {
+            println(s"Bust! The value of your hand is ${hand.value}.")
+            continue = false
+          }
+
+        case "stand" =>
+          continue = false
+          println(s"You chose to stand. The value of your hand value is ${hand.value}.")
+
+        case _ =>
+          println("Invalid choice, please type 'hit' or 'stand'.")
+      }
+    }
   }
 }
 

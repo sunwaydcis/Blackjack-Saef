17e7670366cdccbaa0aa99572683bf53f8500c94
niceguysaef <138495045+niceguysaef@users.noreply.github.com>
2024-08-17 03:53:14 +0800

    Update BlackjackGame.scala
    noticed a problem with the game logic during testing where the player would win even though their hand value was lower than the dealer. edited the winning and losing conditions logic to fix that. Hid the dealers second card before player finishes their turn to more accurately replicate blackjack game. Also added a print to showcase the value of players and dealer hand after all turns are finished for better clarity

--- src/main/scala/BlackjackGame.scala
+++ src/main/scala/BlackjackGame.scala
@@ -13,17 +13,31 @@ class BlackjackGame {
     // Show player's hand
     println(s"Player's hand: ${player.hand}")
     // Show both of the dealer's cards
-    println(s"Dealer's hand: ${dealer.hand}")
+    println(s"Dealer's hand: ${dealer.hand.getCards.head}, [Hidden Card]")
 
     player.playTurn(deck)
     dealer.playTurn(deck)
 
     determineWinner()
   }
+  
   def determineWinner(): Unit = {
-    if (player.hand.value > 21) println("Player busts! Dealer wins.")
-    else if (dealer.hand.value > 21 || player.hand.value > dealer.hand.value) println("Player wins!")
-    else if (player.hand.value == dealer.hand.value) println("It's a tie!")
-    else println("Dealer wins!")
+    val playerValue = player.hand.value
+    val dealerValue = dealer.hand.value
+
+    println(s"Final Player hand value: $playerValue")
+    println(s"Final Dealer hand value: $dealerValue")
+
+    if (playerValue > 21) {
+      println("Player busts! Dealer wins.")
+    } else if (dealerValue > 21) {
+      println("Dealer busts! Player wins.")
+    } else if (playerValue > dealerValue) {
+      println("Player wins!")
+    } else if (playerValue < dealerValue) {
+      println("Dealer wins!")
+    } else {
+      println("It's a tie!")
+    }
   }
 }

44803b135e9983e6d0558f112ab4098ecfe3a111
niceguysaef <138495045+niceguysaef@users.noreply.github.com>
2024-08-18 02:21:31 +0800

    Update MainApp.scala
    Main app to run the code

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,1 +1,24 @@
+package ch.makery.blackjack
 
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafxml.core.{FXMLView, NoDependencyResolver}
+import scalafx.scene.layout.AnchorPane
+
+object MainApp extends JFXApp {
+
+  val resource = getClass.getResource("/interface/MainMenu.fxml")
+  if (resource == null) {
+    throw new RuntimeException("Could not load FXML file")
+  }
+
+  val root = FXMLView(resource, NoDependencyResolver)
+
+  val scalaRoot = new AnchorPane(root.asInstanceOf[javafx.scene.layout.AnchorPane])
+
+  stage = new PrimaryStage {
+    title = "Main Menu"
+    scene = new Scene(scalaRoot, 800, 550) 
+  }
+}

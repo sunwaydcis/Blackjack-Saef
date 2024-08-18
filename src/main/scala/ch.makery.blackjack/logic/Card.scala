a97f9b60c5f43e6c7de2188d31ac19b66eac8dee
niceguysaef <138495045+niceguysaef@users.noreply.github.com>
2024-08-18 21:06:50 +0800

    Create Card.scala
    used to create the cards for the blackjack game and handles their value, suit and image

---
+++ src/main/scala/ch.makery.blackjack/logic/Card.scala
@@ -0,0 +1,9 @@
+package ch.makery.blackjack.logic
+
+class Card(val value: Int, val suit: String, val imagePath: String) {
+  override def toString: String = s"Card(value: $value, suit: $suit, image: $imagePath)"
+}
+
+object Card {
+  def apply(value: Int, suit: String, imagePath: String): Card = new Card(value, suit, imagePath)
+}

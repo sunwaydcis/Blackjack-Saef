e901f3d0648019a2f3630ec8dfe56bcce5079c66
niceguysaef <138495045+niceguysaef@users.noreply.github.com>
2024-08-18 21:12:53 +0800

    Update MainMenuController.scala
    added action for the start game button so that when button is pressed game is started

--- src/main/scala/ch.makery.blackjack/controller/MainMenuController.scala
+++ src/main/scala/ch.makery.blackjack/controller/MainMenuController.scala
@@ -4,15 +4,25 @@ import scalafx.Includes._
 import scalafx.scene.control.Button
 import scalafx.scene.Scene
 import scalafxml.core.{FXMLView, NoDependencyResolver}
-import scalafx.stage.Stage // Use ScalaFX Stage
+import scalafx.stage.Stage
 import scalafx.scene.layout.AnchorPane
 import scalafxml.core.macros.sfxml
 
 @sfxml
 class MainMenuController(val StartGameButton: Button, val StartGameButton1: Button) {
-
+  
   StartGameButton.onAction = handle {
-    println("Hello")
+    val resource = getClass.getResource("/interface/Game.fxml")
+    if (resource == null) {
+      throw new RuntimeException("Could not load FXML file for Game")
+    }
+
+    val root = FXMLView(resource, NoDependencyResolver)
+    val scalaRoot = new AnchorPane(root.asInstanceOf[javafx.scene.layout.AnchorPane])
+    
+    val stage = StartGameButton.getScene.getWindow.asInstanceOf[javafx.stage.Stage]
+    val scalaStage = new Stage(stage) 
+    scalaStage.scene = new Scene(scalaRoot, 800, 550)
   }
 
   StartGameButton1.onAction = handle {
@@ -25,7 +35,7 @@ class MainMenuController(val StartGameButton: Button, val StartGameButton1: Butt
     val scalaRoot = new AnchorPane(root.asInstanceOf[javafx.scene.layout.AnchorPane])
 
     val stage = StartGameButton1.getScene.getWindow.asInstanceOf[javafx.stage.Stage]
-    val scalaStage = new Stage(stage) // Wrap JavaFX Stage in ScalaFX Stage
+    val scalaStage = new Stage(stage)
     scalaStage.scene = new Scene(scalaRoot, 800, 550)
   }
 }

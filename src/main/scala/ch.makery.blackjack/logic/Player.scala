f2a1953da347718a14aba341fc024b0b3a127b00
niceguysaef <138495045+niceguysaef@users.noreply.github.com>
2024-08-20 19:41:27 +0800

    Update Player.scala

--- src/main/scala/ch.makery.blackjack/logic/Player.scala
+++ src/main/scala/ch.makery.blackjack/logic/Player.scala
@@ -1,12 +1,15 @@
 package ch.makery.blackjack.logic
 
+//class for player
 class Player {
   private var cards: List[Card] = List()
 
+  //adds card to player hand
   def receiveCard(card: Card): Unit = {
     cards = cards :+ card
   }
 
+  //counts value of player hand
   def handValue: Int = {
     var sum = 0
     var aceCount = 0
@@ -16,15 +19,20 @@ class Player {
       if (card.value == 11) aceCount += 1
     }
 
+    //changes value of ace from 11 to 1 if player or dealer hand more than 21
     while (sum > 21 && aceCount > 0) {
       sum -= 10
       aceCount -= 1
     }
-
     sum
   }
 
+  //checks if player bust
   def isBusted: Boolean = handValue > 21
+
+  //reset player hand
   def resetHand(): Unit = cards = List()
+
+  //return list of card
   def getCards: List[Card] = cards
 }

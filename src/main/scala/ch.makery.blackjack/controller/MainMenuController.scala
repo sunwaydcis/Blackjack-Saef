65cefc7140dacbf2f7e827c24c71ec10a7669320
niceguysaef <138495045+niceguysaef@users.noreply.github.com>
2024-08-20 19:38:36 +0800

    Update MainMenuController.scala

--- src/main/scala/ch.makery.blackjack/controller/MainMenuController.scala
+++ src/main/scala/ch.makery.blackjack/controller/MainMenuController.scala
@@ -10,8 +10,10 @@ import scalafxml.core.macros.sfxml
 
 @sfxml
 class MainMenuController(val StartGameButton: Button, val StartGameButton1: Button) {
-  
+
+  //handles action when start game button clicked
   StartGameButton.onAction = handle {
+    //loads game.fxml
     val resource = getClass.getResource("/interface/Game.fxml")
     if (resource == null) {
       throw new RuntimeException("Could not load FXML file for Game")
@@ -19,12 +21,13 @@ class MainMenuController(val StartGameButton: Button, val StartGameButton1: Butt
 
     val root = FXMLView(resource, NoDependencyResolver)
     val scalaRoot = new AnchorPane(root.asInstanceOf[javafx.scene.layout.AnchorPane])
-    
+
     val stage = StartGameButton.getScene.getWindow.asInstanceOf[javafx.stage.Stage]
-    val scalaStage = new Stage(stage) 
+    val scalaStage = new Stage(stage)
     scalaStage.scene = new Scene(scalaRoot, 800, 550)
   }
 
+  //handles action for how to play button
   StartGameButton1.onAction = handle {
     val resource = getClass.getResource("/interface/HowToPlay.fxml")
     if (resource == null) {

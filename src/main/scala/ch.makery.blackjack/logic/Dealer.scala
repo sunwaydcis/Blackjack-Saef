c10ed640f3cd981fb6d8a965c4f4a58327bc0e1a
niceguysaef <138495045+niceguysaef@users.noreply.github.com>
2024-08-20 19:40:20 +0800

    Update Dealer.scala

--- src/main/scala/ch.makery.blackjack/logic/Dealer.scala
+++ src/main/scala/ch.makery.blackjack/logic/Dealer.scala
@@ -1,5 +1,6 @@
 package ch.makery.blackjack.logic
 
+//handles dealer logic extending from player making sure dealer draws a card if their hand value less than 17
 class Dealer extends Player {
   def shouldDrawCard: Boolean = handValue < 17
 }

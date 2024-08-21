158e221e6a6ae46d803fd30f0dde2fed9ea99b34
niceguysaef <138495045+niceguysaef@users.noreply.github.com>
2024-08-22 02:12:39 +0800

    Update Dealer.scala
    included polymorphism

--- src/main/scala/ch.makery.blackjack/logic/Dealer.scala
+++ src/main/scala/ch.makery.blackjack/logic/Dealer.scala
@@ -2,5 +2,12 @@ package ch.makery.blackjack.logic
 
 //handles dealer logic extending from player making sure dealer draws a card if their hand value less than 17
 class Dealer extends Player {
+
+  //override receivecard to include specific logic for dealer
+  override def receiveCard(card: Card): Unit = {
+    println(s"Dealer receives card: $card")
+    super.receiveCard(card)
+  }
+
   def shouldDrawCard: Boolean = handValue < 17
 }
